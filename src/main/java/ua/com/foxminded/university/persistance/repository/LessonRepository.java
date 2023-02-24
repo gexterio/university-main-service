13cb8e4389243271cfdf02d8d3b327f9f17a6cf6
foxstudent102662 <gexter.shell@gmail.com>
2023-02-24 18:25:57 +0400

    rework LessonRepository class

--- src/main/java/ua/com/foxminded/university/persistance/repository/LessonRepository.java
+++ src/main/java/ua/com/foxminded/university/persistance/repository/LessonRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ua.com.foxminded.university.persistance.model.LessonEntity;
 
-import java.util.Date;
+import java.time.ZonedDateTime;
 import java.util.List;
 
 @Repository
@@ -15,23 +15,12 @@ public interface LessonRepository extends JpaRepository<LessonEntity, Long> {
             "join GroupEntity g on s.group.id = g.id " +
             "join LessonEntity l on g.id = l.group.id " +
             "where s.id = :id " +
-            "and date_trunc('day', l.startTime) = :date")
-    List<LessonEntity> findAllLessonsForDayByStudentIdAndDate(Long id, Date date);
-
-    @Query(value = "select l from StudentEntity s " +
-            "join fetch GroupEntity g on s.group.id = g.id " +
-            "join fetch LessonEntity l on g.id = l.group.id " +
-            "where s.id= :id " +
-            "and date_trunc('month', l.startTime) = :date")
-    List<LessonEntity> findAllLessonsForMonthByStudentIdAndDate(Long id, Date date);
+            "and l.startTime between :from and :to")
+    List<LessonEntity> findAllLessonsByStudentIdAndDate(Long id, ZonedDateTime from, ZonedDateTime to);
 
     @Query(value = "select l from TeacherEntity t " +
             "join fetch LessonEntity l on l.teacher.id=t.id " +
-            "where t.id = :id and date_trunc('day', l.startTime) = :date")
-    List<LessonEntity> findAllLessonsForDayByTeacherIdAndDate(Long id, Date date);
-
-    @Query(value = "select l from TeacherEntity t " +
-            "join fetch LessonEntity l on l.teacher.id=t.id " +
-            "where t.id = :id and date_trunc('month', l.startTime) = :date")
-    List<LessonEntity> findAllLessonsForMonthByTeacherIdAndDate(Long id, Date date);
+            "where t.id = :id " +
+            "and l.startTime between :from and :to")
+    List<LessonEntity> findAllLessonsByTeacherIdAndDate(Long id, ZonedDateTime from, ZonedDateTime to);
 }
