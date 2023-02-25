061311462bcec83df5073c76e3a02b3639bb3559
foxstudent102662 <gexter.shell@gmail.com>
2023-02-25 12:55:39 +0400

    empty tests

---
+++ src/test/java/ua/com/foxminded/university/persistance/repository/StudentRepositoryTest.java
@@ -0,0 +1,11 @@
+package ua.com.foxminded.university.persistance.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+class StudentRepositoryTest extends RepositoryTestBase {
+
+    @Autowired
+    StudentRepository repository;
+
+
+}
---
+++ src/test/java/ua/com/foxminded/university/persistance/repository/TeacherRepositoryTest.java
@@ -0,0 +1,13 @@
+package ua.com.foxminded.university.persistance.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+class TeacherRepositoryTest extends RepositoryTestBase {
+
+    @Autowired
+    TeacherRepository repository;
+
+
+}
+
+
