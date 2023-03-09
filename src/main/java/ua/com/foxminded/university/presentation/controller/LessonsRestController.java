c3a10919f5f50763a882a92097782b9fdebf4e62
foxstudent102662 <gexter.shell@gmail.com>
2023-03-09 10:56:03 +0400

    remove unneeded methods

--- src/main/java/ua/com/foxminded/university/presentation/controller/LessonsRestController.java
+++ src/main/java/ua/com/foxminded/university/presentation/controller/LessonsRestController.java
@@ -4,15 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 import ua.com.foxminded.university.consumer.dto.LessonDTO;
@@ -38,32 +34,10 @@ public class LessonsRestController {
         return service.findAll(pageable);
     }
 
-    @GetMapping("/{id}")
-    public LessonDTO findById(@PathVariable("id") Long id) {
-        return service.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
-    }
-
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public LessonDTO create(@RequestBody LessonDTO lesson) {
-        return service.create(lesson);
-    }
-
-    @PutMapping("/{id}")
-    public LessonDTO update(@PathVariable("id") Long id, @RequestBody LessonDTO lesson) {
-        return service.update(id, lesson)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
-    }
-
-    @DeleteMapping("/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void delete(@PathVariable("id") Long id) {
-        if (!service.delete(id))
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-    }
-
     @GetMapping("/students/{id}/lessons")
-    public List<LessonDTO> findLessonsForStudent(@PathVariable("id") Long id, @RequestParam("range") String range, @RequestBody String isoDate) {
+    public List<LessonDTO> findLessonsForStudent(@PathVariable("id") Long id,
+                                                 @RequestParam("range") String range,
+                                                 @RequestBody String isoDate) {
         if (range.equals("day")) {
             ZonedDateTime day = ZonedDateTime.parse(isoDate, DateTimeFormatter.ISO_ZONED_DATE_TIME);
             return service.findLessonsForStudentForDay(id, day);
@@ -75,13 +49,15 @@ public class LessonsRestController {
 
 
     @GetMapping("/teachers/{id}/lessons")
-    public List<LessonDTO> findLessonsForTeacher(@PathVariable("id") Long id, @RequestParam("range") String range, @RequestBody String isoDate) {
+    public List<LessonDTO> findLessonsForTeacher(@PathVariable("id") Long id,
+                                                 @RequestParam("range") String range,
+                                                 @RequestBody String isoDate) {
         if (range.equals("day")) {
             ZonedDateTime day = ZonedDateTime.parse(isoDate, DateTimeFormatter.ISO_ZONED_DATE_TIME);
             return service.findLessonsForTeacherForDay(id, day);
         } else if (range.equals("month")) {
             ZonedDateTime month = ZonedDateTime.parse(isoDate, DateTimeFormatter.ISO_ZONED_DATE_TIME);
             return service.findLessonsForTeacherForMonth(id, month);
-        }else throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        } else throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
     }
 }
