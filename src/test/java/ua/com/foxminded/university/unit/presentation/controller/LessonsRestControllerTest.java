7905d46941bbe3f7e042e0e68fa0460a4372e0df
foxstudent102662 <gexter.shell@gmail.com>
2023-03-11 11:33:04 +0400

    split methods for every once test case.

--- src/test/java/ua/com/foxminded/university/unit/presentation/controller/LessonsRestControllerTest.java
+++ src/test/java/ua/com/foxminded/university/unit/presentation/controller/LessonsRestControllerTest.java
@@ -1,16 +1,13 @@
-package ua.com.foxminded.university.presentation.controller;
+package ua.com.foxminded.university.unit.presentation.controller;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import ua.com.foxminded.university.consumer.dto.LessonDTO;
+import ua.com.foxminded.university.presentation.controller.LessonsRestController;
 import ua.com.foxminded.university.service.LessonService;
 
 import java.time.ZonedDateTime;
@@ -34,56 +31,15 @@ class LessonsRestControllerTest {
     @Autowired
     MockMvc mockMvc;
 
-
     @Test
-    void findAll_returnedPageOfDto_Exists() throws Exception {
-        List<LessonDTO> lessonList = List.of(
-                new LessonDTO.Builder().setId(1L).setName("Name").build(),
-                new LessonDTO.Builder().setId(2L).setName("Name1").build(),
-                new LessonDTO.Builder().setId(3L).setName("Name2").build());
-        Pageable pageable = PageRequest.of(0, 20);
-        Page<LessonDTO> page = new PageImpl<>(lessonList, pageable, lessonList.size());
-        when(service.findAll(pageable)).thenReturn(page);
-        mockMvc.perform(get("/api/v1/lessons")
-                        .param("page", String.valueOf(pageable.getPageNumber()))
-                        .param("size", String.valueOf(pageable.getPageSize())))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.content.length()").value(lessonList.size()))
-                .andExpect(jsonPath("$.content[0].name").value("Name"))
-                .andExpect(jsonPath("$.content[1].name").value("Name1"))
-                .andExpect(jsonPath("$.content[2].name").value("Name2"))
-                .andDo(print());
-    }
-
-    @Test
-    void findAll_returnedEmptyPageOfDto_NotExists() throws Exception {
-        List<LessonDTO> lessonList = new ArrayList<>();
-        Pageable pageable = PageRequest.of(0, 20);
-        Page<LessonDTO> page = new PageImpl<>(lessonList, pageable, lessonList.size());
-        when(service.findAll(pageable)).thenReturn(page);
-        mockMvc.perform(get("/api/v1/lessons")
-                        .param("page", String.valueOf(pageable.getPageNumber()))
-                        .param("size", String.valueOf(pageable.getPageSize())))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.content.length()").value(lessonList.size()))
-                .andDo(print());
-    }
-
-    @Test
-    void findLessonsForStudent_returnedListOfLessons_Exists() throws Exception {
+    void findLessonsForStudent_returnedListOfLessons_ExistsAndRangeIsDay() throws Exception {
         String isoDate = "2023-02-13T14:53:34.396+00:00";
         ZonedDateTime date = ZonedDateTime.parse(isoDate, DateTimeFormatter.ISO_ZONED_DATE_TIME);
         Long id = 1L;
         List<LessonDTO> lessonsForDay = List.of(
                 new LessonDTO.Builder().setId(1L).setName("LessonOne").build()
         );
-        List<LessonDTO> lessonsForMonth = List.of(
-                new LessonDTO.Builder().setId(1L).setName("LessonOne").build(),
-                new LessonDTO.Builder().setId(2L).setName("LessonTwo").build(),
-                new LessonDTO.Builder().setId(3L).setName("LessonThree").build()
-        );
         when(service.findLessonsForStudentForDay(id, date)).thenReturn(lessonsForDay);
-        when(service.findLessonsForStudentForMonth(id, date)).thenReturn(lessonsForMonth);
 
         mockMvc.perform(get("/api/v1/students/" + id + "/lessons")
                         .param("range", "day")
@@ -93,6 +49,19 @@ class LessonsRestControllerTest {
                 .andExpect(jsonPath("$.[0].id").value("1"))
                 .andExpect(jsonPath("$.[0].name").value("LessonOne"))
                 .andDo(print());
+    }
+
+    @Test
+    void findLessonsForStudent_returnedListOfLessons_ExistsAndRangeIsMonth() throws Exception {
+        String isoDate = "2023-02-13T14:53:34.396+00:00";
+        ZonedDateTime date = ZonedDateTime.parse(isoDate, DateTimeFormatter.ISO_ZONED_DATE_TIME);
+        Long id = 1L;
+        List<LessonDTO> lessonsForMonth = List.of(
+                new LessonDTO.Builder().setId(1L).setName("LessonOne").build(),
+                new LessonDTO.Builder().setId(2L).setName("LessonTwo").build(),
+                new LessonDTO.Builder().setId(3L).setName("LessonThree").build()
+        );
+        when(service.findLessonsForStudentForMonth(id, date)).thenReturn(lessonsForMonth);
 
         mockMvc.perform(get("/api/v1/students/" + id + "/lessons")
                         .param("range", "month")
@@ -109,20 +78,14 @@ class LessonsRestControllerTest {
     }
 
     @Test
-    void findLessonsForTeacher_returnedListOfLessons_Exists() throws Exception {
+    void findLessonsForTeacher_returnedListOfLessons_ExistsAndRangeIsDay() throws Exception {
         String isoDate = "2023-02-13T14:53:34.396+00:00";
         ZonedDateTime date = ZonedDateTime.parse(isoDate, DateTimeFormatter.ISO_ZONED_DATE_TIME);
         Long id = 1L;
         List<LessonDTO> lessonsForDay = List.of(
                 new LessonDTO.Builder().setId(1L).setName("LessonOne").build()
         );
-        List<LessonDTO> lessonsForMonth = List.of(
-                new LessonDTO.Builder().setId(1L).setName("LessonOne").build(),
-                new LessonDTO.Builder().setId(2L).setName("LessonTwo").build(),
-                new LessonDTO.Builder().setId(3L).setName("LessonThree").build()
-        );
         when(service.findLessonsForTeacherForDay(id, date)).thenReturn(lessonsForDay);
-        when(service.findLessonsForTeacherForMonth(id, date)).thenReturn(lessonsForMonth);
 
         mockMvc.perform(get("/api/v1/teachers/" + id + "/lessons")
                         .param("range", "day")
@@ -132,6 +95,19 @@ class LessonsRestControllerTest {
                 .andExpect(jsonPath("$.[0].id").value("1"))
                 .andExpect(jsonPath("$.[0].name").value("LessonOne"))
                 .andDo(print());
+    }
+
+    @Test
+    void findLessonsForTeacher_returnedListOfLessons_ExistsAndRangeISMonth() throws Exception {
+        String isoDate = "2023-02-13T14:53:34.396+00:00";
+        ZonedDateTime date = ZonedDateTime.parse(isoDate, DateTimeFormatter.ISO_ZONED_DATE_TIME);
+        Long id = 1L;
+        List<LessonDTO> lessonsForMonth = List.of(
+                new LessonDTO.Builder().setId(1L).setName("LessonOne").build(),
+                new LessonDTO.Builder().setId(2L).setName("LessonTwo").build(),
+                new LessonDTO.Builder().setId(3L).setName("LessonThree").build()
+        );
+        when(service.findLessonsForTeacherForMonth(id, date)).thenReturn(lessonsForMonth);
 
         mockMvc.perform(get("/api/v1/teachers/" + id + "/lessons")
                         .param("range", "month")
@@ -148,14 +124,12 @@ class LessonsRestControllerTest {
     }
 
     @Test
-    void findLessonsForStudent_returnedEmptyList_NotExists() throws Exception {
+    void findLessonsForStudent_returnedEmptyList_NotExistsAndRangeIsDay() throws Exception {
         String isoDate = "2023-02-13T14:53:34.396+00:00";
         ZonedDateTime date = ZonedDateTime.parse(isoDate, DateTimeFormatter.ISO_ZONED_DATE_TIME);
         Long id = 1L;
         List<LessonDTO> lessonsForDay = new ArrayList<>();
-        List<LessonDTO> lessonsForMonth = new ArrayList<>();
         when(service.findLessonsForStudentForDay(id, date)).thenReturn(lessonsForDay);
-        when(service.findLessonsForStudentForMonth(id, date)).thenReturn(lessonsForMonth);
 
         mockMvc.perform(get("/api/v1/students/" + id + "/lessons")
                         .param("range", "day")
@@ -163,6 +137,15 @@ class LessonsRestControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.length()").value(lessonsForDay.size()))
                 .andDo(print());
+    }
+
+    @Test
+    void findLessonsForStudent_returnedEmptyList_NotExistsAndRangeIsMonth() throws Exception {
+        String isoDate = "2023-02-13T14:53:34.396+00:00";
+        ZonedDateTime date = ZonedDateTime.parse(isoDate, DateTimeFormatter.ISO_ZONED_DATE_TIME);
+        Long id = 1L;
+        List<LessonDTO> lessonsForMonth = new ArrayList<>();
+        when(service.findLessonsForStudentForMonth(id, date)).thenReturn(lessonsForMonth);
 
         mockMvc.perform(get("/api/v1/students/" + id + "/lessons")
                         .param("range", "month")
@@ -173,14 +156,12 @@ class LessonsRestControllerTest {
     }
 
     @Test
-    void findLessonsForTeachers_returnedEmptyList_NotExists() throws Exception {
+    void findLessonsForTeachers_returnedEmptyList_NotExistsAndRangeIsDay() throws Exception {
         String isoDate = "2023-02-13T14:53:34.396+00:00";
         ZonedDateTime date = ZonedDateTime.parse(isoDate, DateTimeFormatter.ISO_ZONED_DATE_TIME);
         Long id = 1L;
         List<LessonDTO> lessonsForDay = new ArrayList<>();
-        List<LessonDTO> lessonsForMonth = new ArrayList<>();
         when(service.findLessonsForTeacherForDay(id, date)).thenReturn(lessonsForDay);
-        when(service.findLessonsForTeacherForMonth(id, date)).thenReturn(lessonsForMonth);
 
         mockMvc.perform(get("/api/v1/students/" + id + "/lessons")
                         .param("range", "day")
@@ -188,6 +169,16 @@ class LessonsRestControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.length()").value(lessonsForDay.size()))
                 .andDo(print());
+    }
+
+
+    @Test
+    void findLessonsForTeachers_returnedEmptyList_NotExistsAndRangeIsMonth() throws Exception {
+        String isoDate = "2023-02-13T14:53:34.396+00:00";
+        ZonedDateTime date = ZonedDateTime.parse(isoDate, DateTimeFormatter.ISO_ZONED_DATE_TIME);
+        Long id = 1L;
+        List<LessonDTO> lessonsForMonth = new ArrayList<>();
+        when(service.findLessonsForTeacherForMonth(id, date)).thenReturn(lessonsForMonth);
 
         mockMvc.perform(get("/api/v1/students/" + id + "/lessons")
                         .param("range", "month")
