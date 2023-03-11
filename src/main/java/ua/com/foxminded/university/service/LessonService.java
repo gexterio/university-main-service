2363ca28708c8e6dd2060071b8d6bbbec12a3792
foxstudent102662 <gexter.shell@gmail.com>
2023-03-11 11:05:06 +0400

    cherry-pick from service: "remove CRUD methods"

--- src/main/java/ua/com/foxminded/university/service/LessonService.java
+++ src/main/java/ua/com/foxminded/university/service/LessonService.java
@@ -1,10 +1,7 @@
 package ua.com.foxminded.university.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import ua.com.foxminded.university.consumer.dto.LessonDTO;
 import ua.com.foxminded.university.persistance.repository.LessonRepository;
 import ua.com.foxminded.university.util.modelmapper.LessonMapper;
@@ -12,7 +9,6 @@ import ua.com.foxminded.university.util.modelmapper.LessonMapper;
 import java.time.ZonedDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -25,43 +21,6 @@ public class LessonService {
     public LessonService(LessonRepository repository, LessonMapper mapper) {
         this.repository = repository;
         this.mapper = mapper;
-    }
-
-    public Page<LessonDTO> findAll(Pageable pageable) {
-        return repository.findAll(pageable)
-                .map(mapper::toDto);
-    }
-
-    public Optional<LessonDTO> findById(Long id) {
-        return repository.findById(id)
-                .map(mapper::toDto);
-    }
-
-    @Transactional
-    public LessonDTO create(LessonDTO dto) {
-        return Optional.of(dto)
-                .map(mapper::toEntity)
-                .map(repository::save)
-                .map(mapper::toDto)
-                .orElseThrow();
-    }
-
-    @Transactional
-    public Optional<LessonDTO> update(Long id, LessonDTO lesson) {
-        return repository.findById(id)
-                .map(entity -> mapper.toEntity(lesson))
-                .map(repository::saveAndFlush)
-                .map(mapper::toDto);
-    }
-
-    public boolean delete(Long id) {
-        return repository.findById(id)
-                .map(entity -> {
-                    repository.delete(entity);
-                    repository.flush();
-                    return true;
-                })
-                .orElse(false);
     }
 
     public List<LessonDTO> findLessonsForStudentForDay(Long id, ZonedDateTime day) {
