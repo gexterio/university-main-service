770d55c589d586682964c236cb81f88169bf570f
foxstudent102662 <gexter.shell@gmail.com>
2023-03-19 16:31:51 +0400

    add AOP Logging of Services

---
+++ src/main/java/ua/com/foxminded/university/aspect/LoggingAspect.java
@@ -0,0 +1,107 @@
+package ua.com.foxminded.university.aspect;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class LoggingAspect {
+
+    private final Logger log = LogManager.getLogger(this.getClass());
+
+
+    @Around("PointCuts.isServiceLayer() && PointCuts.isAnyFindAllServiceMethod()" +
+            "&& args(pageable)")
+    public Object addLoggingAroundServiceFindAllMethod(ProceedingJoinPoint joinPoint, Pageable pageable) throws Throwable {
+        long startTime = System.currentTimeMillis();
+        log.info("Trying to get {} entities from Database...", pageable.getPageSize());
+        try {
+            Object result = joinPoint.proceed();
+            long time = System.currentTimeMillis() - startTime;
+            int size = 0;
+            String type = "unknown";
+            if (result instanceof PageImpl) {
+                size = ((PageImpl<?>) result).getSize();
+                type = ((PageImpl<?>) result).getContent().get(0).getClass().getSimpleName();
+            }
+            log.info("Entities were received in {}ms. Type: {}. Count: {}.", time, type, size);
+            return result;
+        } catch (Throwable e) {
+            log.warn("Can't get entities. Cause: {}", e.getMessage());
+            throw e;
+        }
+    }
+
+
+    @Around("PointCuts.isServiceLayer() && PointCuts.isAnyFindByIdServiceMethod()" +
+            "&& args(id)")
+    public Object addLoggingAroundServiceFindByIdMethod(ProceedingJoinPoint joinPoint, Long id) throws Throwable {
+        long startTime = System.currentTimeMillis();
+        log.info("Trying to get entity from Database by ID: {}...", id);
+        try {
+            Object result = joinPoint.proceed();
+            long time = System.currentTimeMillis() - startTime;
+            log.info("Entity by ID: {} was found in {} ms: [{}]", id, time, result);
+            return result;
+        } catch (Throwable e) {
+            log.warn("Can't get entity by ID = {}. Cause: {}", id, e.getMessage());
+            throw e;
+        }
+    }
+
+    @Around("PointCuts.isServiceLayer() && PointCuts.isAnyCreateServiceMethod()" +
+            "&& args(dto)")
+    public Object addLoggingAroundServiceCreateMethod(ProceedingJoinPoint joinPoint, Object dto) throws Throwable {
+        long startTime = System.currentTimeMillis();
+        log.info("Trying to add entity into Database: {}", dto);
+        try {
+            Object result = joinPoint.proceed();
+            long time = System.currentTimeMillis() - startTime;
+            log.info("Entity added in {}ms: {}", time, result);
+            return result;
+        } catch (Throwable e) {
+            log.warn("Can't add entity into Database. Cause: {}. Entity: {}", e.getMessage(), dto);
+            throw e;
+        }
+    }
+
+    @Around("PointCuts.isServiceLayer() && PointCuts.isAnyUpdateServiceMethod()" +
+            "&& args(dto)")
+    public Object addLoggingAroundServiceUpdateMethod(ProceedingJoinPoint joinPoint, Object dto) throws Throwable {
+        long startTime = System.currentTimeMillis();
+        log.info("Trying to update entity in Database: {}", dto);
+        try {
+            Object result = joinPoint.proceed();
+            long time = System.currentTimeMillis() - startTime;
+            log.info("Entity updated in {}ms: {}", time, result);
+            return result;
+        } catch (Throwable e) {
+            log.warn("Can't update entity in Database. Cause: {}. Entity: {}", e.getMessage(), dto);
+            throw e;
+        }
+    }
+
+    @Around("PointCuts.isServiceLayer() && PointCuts.isAnyDeleteServiceMethod()" +
+            "&& args(id)")
+    public Object addLoggingAroundServiceDeleteMethod(ProceedingJoinPoint joinPoint, Long id) throws Throwable {
+        long startTime = System.currentTimeMillis();
+        log.info("Trying to delete entity from Database: ID = {}", id);
+        try {
+            Object result = joinPoint.proceed();
+            long time = System.currentTimeMillis() - startTime;
+            log.info("Entity deleted in {}ms: ID ={}, successfully: {}", time, id, result);
+            return result;
+        } catch (Throwable e) {
+            log.warn("Can't delete entity from Database. Cause: {}. ID: {}", e.getMessage(), id);
+            throw e;
+        }
+    }
+
+
+}
---
+++ src/main/java/ua/com/foxminded/university/aspect/PointCuts.java
@@ -0,0 +1,34 @@
+package ua.com.foxminded.university.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class PointCuts {
+
+    @Pointcut("@within(org.springframework.stereotype.Service)")
+    public void isServiceLayer() {
+    }
+
+    @Pointcut("execution(public * findById(*))throws *NotFoundException")
+    public void isAnyFindByIdServiceMethod() {
+    }
+
+    @Pointcut("execution(public  * findAll(*))")
+    public void isAnyFindAllServiceMethod() {
+    }
+
+    @Pointcut("execution(public * create(*))")
+    public void isAnyCreateServiceMethod() {
+    }
+
+    @Pointcut("execution(public * update(*))")
+    public void isAnyUpdateServiceMethod() {
+    }
+
+    @Pointcut("execution(public * delete(*))")
+    public void isAnyDeleteServiceMethod() {
+    }
+}
