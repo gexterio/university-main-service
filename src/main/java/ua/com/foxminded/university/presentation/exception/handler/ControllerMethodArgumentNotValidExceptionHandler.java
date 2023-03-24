89b558f37e35716137e7fe317bd7c4dc2562d094
foxstudent102662 <gexter.shell@gmail.com>
2023-03-24 17:45:50 +0400

    refactoring

--- src/main/java/ua/com/foxminded/university/presentation/exception/handler/ControllerMethodArgumentNotValidExceptionHandler.java
+++ src/main/java/ua/com/foxminded/university/presentation/exception/handler/ControllerMethodArgumentNotValidExceptionHandler.java
@@ -2,14 +2,14 @@ package ua.com.foxminded.university.presentation.exception.handler;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintViolationException;
 
 @ControllerAdvice
 public class ControllerMethodArgumentNotValidExceptionHandler {
@@ -17,21 +17,29 @@ public class ControllerMethodArgumentNotValidExceptionHandler {
     Logger log = LogManager.getLogger(this.getClass());
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Object> handleException(MethodArgumentNotValidException e, HttpServletRequest request) {
-        StringBuilder errors = new StringBuilder();
+    public ResponseEntity<Object> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        StringBuilder msg = new StringBuilder();
         e.getBindingResult().getAllErrors()
-                .forEach(err -> {
-                    if (err.getArguments() == null) {
-                        errors.append(String.format("%s: %s%n",
-                                "unknown",
-                                err.getDefaultMessage()));
-                    } else errors.append(String.format("%s: %s%n",
-                            ((DefaultMessageSourceResolvable) err.getArguments()[0]).getDefaultMessage(),
-                            err.getDefaultMessage()));
+                .stream()
+                .map(FieldError.class::cast)
+                .forEach(er -> msg.append(String.format("%s = %s. %s%n",
+                        er.getField(),
+                        er.getRejectedValue(),
+                        er.getDefaultMessage())).append("\n"));
+        log.info("can't create or update entity! Cause:\n {}", msg);
+        return new ResponseEntity<>(msg.toString(), HttpStatus.BAD_REQUEST);
+    }
 
-                });
-        log.info("can't create or update entity! Cause:\n {}", errors);
-        return new ResponseEntity<>(errors.toString(), HttpStatus.BAD_REQUEST);
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<Object> handleConstraintViolationException(ConstraintViolationException e) {
+        StringBuilder msg = new StringBuilder();
+        e.getConstraintViolations()
+                .forEach(ex ->
+                        msg.append(String.format("value = %s %s",
+                                ex.getInvalidValue(),
+                                ex.getMessage())).append("\n"));
+        log.info("can't get entity! Cause:\n{}", msg);
+        return new ResponseEntity<>(msg.toString(), HttpStatus.BAD_REQUEST);
     }
 
 }
