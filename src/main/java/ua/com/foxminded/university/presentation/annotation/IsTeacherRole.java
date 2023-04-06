e4b0633b3c5809bd5532f580538eb20dc5d3a5ea
foxstudent102662 <gexter.shell@gmail.com>
2023-04-06 16:01:47 +0400

    change using method from hasAuthority to hasRole

--- src/main/java/ua/com/foxminded/university/presentation/annotation/IsTeacherRole.java
+++ src/main/java/ua/com/foxminded/university/presentation/annotation/IsTeacherRole.java
@@ -9,6 +9,6 @@ import java.lang.annotation.Target;
 
 @Target({ElementType.METHOD, ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
-@PreAuthorize("hasAuthority('TEACHER')")
+@PreAuthorize("hasRole('TEACHER')")
 public @interface IsTeacherRole {
 }
