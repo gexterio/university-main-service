e1fbd0b26069fe5aa6edce441f7203ff0b7dc885
foxstudent102662 <gexter.shell@gmail.com>
2023-04-09 13:15:37 +0400

    small refactoring

--- src/main/java/ua/com/foxminded/university/consumer/dto/StudentDTO.java
+++ src/main/java/ua/com/foxminded/university/consumer/dto/StudentDTO.java
@@ -187,7 +187,6 @@ public class StudentDTO {
                 ", firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
                 ", age=" + age +
-                ", group=" + group.getName() +
                 ", course=" + course +
                 ", email='" + email + '\'' +
                 '}';
