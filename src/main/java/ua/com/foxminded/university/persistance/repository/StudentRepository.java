1d250f688118c1cd84afc03f4f7ea3b645ff998e
foxstudent102662 <gexter.shell@gmail.com>
2023-04-19 19:01:46 +0400

    small refactoring

--- src/main/java/ua/com/foxminded/university/persistance/repository/StudentRepository.java
+++ src/main/java/ua/com/foxminded/university/persistance/repository/StudentRepository.java
@@ -4,8 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ua.com.foxminded.university.persistance.model.StudentEntity;
 
+import java.util.Optional;
+
 @Repository
 public interface StudentRepository extends JpaRepository<StudentEntity, Long> {
 
-    StudentEntity findByEmail(String email);
+    Optional<StudentEntity> findByEmail(String email);
 }
--- src/main/java/ua/com/foxminded/university/persistance/repository/TeacherRepository.java
+++ src/main/java/ua/com/foxminded/university/persistance/repository/TeacherRepository.java
@@ -4,7 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ua.com.foxminded.university.persistance.model.TeacherEntity;
 
+import java.util.Optional;
+
 @Repository
 public interface TeacherRepository extends JpaRepository<TeacherEntity, Long> {
 
+    Optional<TeacherEntity> findByEmail(String username);
 }
