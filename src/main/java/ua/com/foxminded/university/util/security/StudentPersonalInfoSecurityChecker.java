760e4fbce33ea23fdc58d6175c52fb948f7a03b6
foxstudent102662 <gexter.shell@gmail.com>
2023-04-19 19:03:43 +0400

    update checking and add the same for teacher

--- src/main/java/ua/com/foxminded/university/util/security/StudentPersonalInfoSecurityChecker.java
+++ src/main/java/ua/com/foxminded/university/util/security/StudentPersonalInfoSecurityChecker.java
@@ -5,24 +5,22 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PathVariable;
-import ua.com.foxminded.university.persistance.repository.StudentRepository;
+import ua.com.foxminded.university.consumer.service.StudentService;
 
 @Component
 public class StudentPersonalInfoSecurityChecker {
 
-    private final StudentRepository studentRepository;
+    private final StudentService studentService;
 
     @Autowired
-    public StudentPersonalInfoSecurityChecker(StudentRepository studentRepository) {
-        this.studentRepository = studentRepository;
+    public StudentPersonalInfoSecurityChecker(StudentService studentService) {
+        this.studentService = studentService;
     }
 
     public boolean checkStudentId(Authentication authentication, @PathVariable Long pathId) {
         User user = (User) authentication.getPrincipal();
-        if (user.getAuthorities().stream().anyMatch(auth -> auth.getAuthority().equals("ROLE_ADMIN"))) {
-            return true;
-        } else if (user.getAuthorities().stream().anyMatch(auth -> auth.getAuthority().equals("ROLE_STUDENT"))) {
-            return pathId.equals(studentRepository.findByEmail(user.getUsername()).getId());
+        if (user.getAuthorities().stream().anyMatch(auth -> auth.getAuthority().equals("ROLE_STUDENT"))) {
+            return pathId.equals(studentService.findByEmail(user.getUsername()).getId());
         }
         return false;
     }
