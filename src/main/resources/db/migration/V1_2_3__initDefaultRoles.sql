c74f72fe8b2c55f121284559d05251360c9e5b09
gexterio <gexterio@gmail.com>
2023-05-18 14:55:18 +0400

    fix issue with authorization

--- src/main/resources/db/migration/V1_2_3__initDefaultRoles.sql
+++ src/main/resources/db/migration/V1_2_3__initDefaultRoles.sql
@@ -1,4 +1,4 @@
 insert into roles (id, name)
-values (1, 'ADMIN'),
-       (2, 'STUDENT'),
-       (3, 'TEACHER');
+values (1, 'ROLE_ADMIN'),
+       (2, 'ROLE_STUDENT'),
+       (3, 'ROLE_TEACHER');
